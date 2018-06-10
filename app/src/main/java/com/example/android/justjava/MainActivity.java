21ba7e2245d952b74b6b70c6415a39f504e077ca
Sid <35898841+clrzr@users.noreply.github.com>
2018-06-10 21:53:15 +0100

    First Version of the app

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int quantity=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int number_of_coffee=quantity;
+        displayPrice(number_of_coffee*700);
+    }
+    public void increment(View view) {
+        quantity=quantity+1;
+        display(quantity);
+    }
+    public void decrement(View view) {
+        quantity=quantity-1;
+        if (quantity<1)
+            quantity=0;
+        display(quantity);
+    }
+    public void reset(View view) {
+        quantity = 0;
+        displayPrice(0);
+        display(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+number);
+    }
+    private void displayPrice(int number) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance(new Locale("en","NG")).format(number));
+    }
+}
