02d2ad5646eef1b512912ea38a9460a4e26b7923
Sid <35898841+clrzr@users.noreply.github.com>
2018-06-11 13:42:16 +0100

    Fixed Java Warning Added Horizontal linear layout to existing linear layout (For button and quantity display.)

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void display(int number) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(""+number);
+        quantityTextView.setText(number);
     }
     private void displayPrice(int number) {
         TextView priceTextView = findViewById(R.id.price_text_view);
